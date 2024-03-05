dc5b5396b25094b04ef75245a846108c4be383cf
ReLLoMine <slaval.125.sl@gmail.com>
2024-03-05 10:17:48 +0300

    some cool features

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import argparse
 def main():
     arg_parser = argparse.ArgumentParser(description="Avito parse CLI app")
     arg_parser.add_argument("url", type=str, help="URL to parse")
-    arg_parser.add_argument("csv_path", type=str, help="CSV file")
-    arg_parser.add_argument("--count", type=int, default=15, help="How many pages to view")
+    arg_parser.add_argument("csv_path", type=str, help="Path to save CSV")
+    arg_parser.add_argument("--count", type=int, default=15, help="How many products to parse")
     arg_parser.add_argument("--max_price", type=int, default=0, help="Max price")
     arg_parser.add_argument("--min_price", type=int, default=0, help="Min price")
     arg_parser.add_argument("--address", type=str, default="", help="Address")
-    arg_parser.add_argument("--log_level", type=int, default=0, help="How many messages to show")
+    arg_parser.add_argument("--log_level", type=int, default=0, help="0 - success and errors, 1 - info, 2 - debug")
     arg_parser.add_argument("--keywords", type=str, default=[], help="Key words", nargs="+")
+    arg_parser.add_argument("--version", action="version", version="Avito Parser 1.0")
+    arg_parser.add_argument("--verbose", action="store_true", help="Verbose mode")  # TODO: Add verbose mode
+    arg_parser.add_argument("--quiet", action="store_true", help="Quiet mode")  # TODO: Add quiet mode
 
     args = arg_parser.parse_args()
 
